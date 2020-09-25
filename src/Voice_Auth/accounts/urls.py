42328908b2c43f1ce31d7611d5e1efe7ede4a6e2
jayeshkarvir5 <jayeshkarvir5@gmail.com>
2020-09-25 20:01:41 +0530

    Logout issue fix 2

--- src/Voice_Auth/accounts/urls.py
+++ src/Voice_Auth/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from .forms import UserLoginForm
+from django.contrib.auth import logout
 
 app_name = 'accounts'
 urlpatterns = [
@@ -10,6 +11,6 @@ urlpatterns = [
     # authentication_form=UserLoginForm), name="login"),
     path('login/<username>', views.LoginView.as_view(), name="login"),
     path('enter/', views.UnView.as_view(), name="enter-un"),
-    path('logout/', views.LogoutView, name="logout"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('logout-page/', views.LogoutPage.as_view(), name="logout_page"),
 ]
